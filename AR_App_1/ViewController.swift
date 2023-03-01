9558366b1cc6c4fecc93329cc7a5a1cfba1e55c2
Greg Ramos <grr5000@live.com>
2023-02-28 20:45:44 -0500

    Initial Commit

---
+++ AR_App_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AR_App_1
+//
+//  Created by Greg Ramos on 2/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
